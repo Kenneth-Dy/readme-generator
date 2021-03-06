747cefc3124998a8565b0795fe2145fcd57a3240
Kenneth Dy <kennethndy@yahoo.com>
2021-03-05 18:40:30 -0800

    deleted and made module file

--- README.md
+++ README.md
@@ -1,25 +1,39 @@
-# READEME generator
+#README generator
+##Description
 
-![license](https://img.shields.io/badge/license-MIT-brightgreen)
+![license](https://img.shields.io/badge/license-GNU%20GPLv3-blue)
 
-MIT License
+It will auto generate a professional readme file based on user input from the command line
+      
+##Table of Contents
 
-Copyright (c) 2021 Kenneth Dy
+[Installation](#Installation)
+[Usage](#Usage)
+[License](#License)
+[Contributing](#Contributing)
+[Questions](#Questions)
+      
+##Installation
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+To install simply clone the repo and use your preferred text editor to install the node files using node.js syntax.
+        
+##Usage
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+To use the project simply run it on the command line with node index.js
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+##License
+
+Refer to the link for the full  license information: [GNU GPLv3](https://choosealicense.com/licenses/gpl-3.0/)
+
+##Contributing
+
+To contribute simply contact me. I put my email in the questions section.
+
+##Questions
+
+GitHub account:
+[Kenneth-Dy](https://github.com/Kenneth-Dy)
+
+If you have questions please contact me through my email: 
+  * kennethndy@yahoo.com
+  
--- index.js
+++ index.js
@@ -1,63 +1,7 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+const licenseChoices = require('./utils/licenseChoices')
 
-const licenseChoices = [
-  {
-    image: 'https://img.shields.io/badge/license-MIT-brightgreen',
-    text: `
-                  MIT License Copyright (c) 2021 Kenneth Dy
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.`
-  },
-  {
-    image: 'https://img.shields.io/badge/license-GNU%20GPLv3-blue',
-    text: `Refer to the link for the full  license information: [GNU GPLv3](https://choosealicense.com/licenses/gpl-3.0/)`
-  },
-  {
-    image: 'https://img.shields.io/badge/license-Unlicense-lightgrey'
-    text:`
-    This is free and unencumbered software released into the public domain.
-
-    Anyone is free to copy, modify, publish, use, compile, sell, or
-    distribute this software, either in source code form or as a compiled
-    binary, for any purpose, commercial or non-commercial, and by any
-    means.
-
-    In jurisdictions that recognize copyright laws, the author or authors
-    of this software dedicate any and all copyright interest in the
-    software to the public domain. We make this dedication for the benefit
-    of the public at large and to the detriment of our heirs and
-    successors. We intend this dedication to be an overt act of
-    relinquishment in perpetuity of all present and future rights to this
-    software under copyright law.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-    IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-    OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-    OTHER DEALINGS IN THE SOFTWARE.
-
-    For more information, please refer to <https://unlicense.org>`
-  }
-]
 
 // array of questions for user
 const questions = [
@@ -94,8 +38,8 @@ const questions = [
   {
     type: 'list',
     name: 'license',
-    message: 'What kind of license do you want?'
-    choices: ['I want it simple and permissive', 'I care about sharing improvements', 'No restrictions']
+    message: 'What kind of license do you want?',
+    choices: ['simple', 'collab', 'No restrictions']
   },
   {
     type: 'input',
@@ -117,65 +61,69 @@ function writeToFile(fileName, data) {
 function init() {
   inquirer.prompt(questions)
     .then(res => {
+      let licenseImage = ''
+      let licenseText = ''
 
-      if (res.license === 'I want it simple and permissive') {
-        let licenseImage = licenseChoices[0].image
-        let licenseText = licenseChoices[0].text
-      }else if (res.license === 'I care about sharing improvements') {
-        let licenseImage = licenseChoices[1].image
-        let licenseText = licenseChoices[1].text
-      }else {
-        let licenseImage = licenseChoices[2].image
-        let licenseText = licenseChoices[2].text
+      switch (res.license) {
+        case 'simple':
+          licenseImage = licenseChoices[0].image
+          licenseText = licenseChoices[0].text
+          break;
+        case 'collab':
+          licenseImage = licenseChoices[1].image
+          licenseText = licenseChoices[1].text
+          break;
+        case 'No restrictions':
+          licenseImage = licenseChoices[2].image
+          licenseText = licenseChoices[2].text
+          break;
       }
 
-      let read = `
-        #${res.title} 
+      let read = `#${res.title}
+##Description
 
-        ##Description
+![license](${licenseImage})
 
-        ![license](${licenseImage})
-
-        ${res.description}
+${res.description}
       
-        ##Table of Contents
+##Table of Contents
 
-          [Installation](#Installation)
-          [Usage](#Usage)
-          [License](#License)
-          [Contributing](#Contributing)
-          [Tests](#Tests)
-          [Questions](#Questions)
+[Installation](#Installation)
+[Usage](#Usage)
+[License](#License)
+[Contributing](#Contributing)
+[Tests](#Tests)
+[Questions](#Questions)
       
-        ##Installation
+##Installation
 
-        ${res.install}
+${res.install}
         
-        ##Usage
+##Usage
 
-        ${res.usage}
+${res.usage}
 
-        ##License
+##License
 
-        ${licenseText}
+${licenseText}
 
-        ##Contributing
+##Contributing
 
-        ${res.contribution}
-        
-        ##Tests
-      
-        ${res.test}
+${res.contribution}
 
-        ##Questions
+##Tests
 
-        GitHub account:
-          * [${res.username}](https://github.com/${res.username})
+${res.test}
 
-        If you have questions please contact me through my email: 
-          * ${res.email}
-         
-      `
+##Questions
+
+GitHub account:
+[${res.username}](https://github.com/${res.username})
+
+If you have questions please contact me through my email: 
+  * ${res.email}
+  
+`
 
       fs.writeFile('./generatedREADME/README.md', read, err => {
         if(err) { console.log(err) }
